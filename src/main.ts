4f655abe69e0a901ed6100adc7c270c8323491ff
Timur <timurtokaev.toombez@gmail.com>
2022-12-25 04:33:22 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { Client } from 'discord.js'
+
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const token = process.env.token
+
+const client = new Client({
+    intents: [
+        'Guilds',
+        'MessageContent',
+        'GuildMembers',
+        'GuildMessages',
+    ]
+});
+
+client.on('ready', (client) => {
+    console.log(`${client.user.username} ready`)
+});
+
+client.login(token);
