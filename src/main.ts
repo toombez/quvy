2aca4c7c5f98f8892601b82f3ee0b17147c7eedf
Timur <timurtokaev.toombez@gmail.com>
2023-01-15 02:30:28 +0300

    Remove Deploy commands script

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import { Client, Collection } from 'discord.js'
+import { Collection } from 'discord.js'
+import PettyClient from './models/PettyClient'
 import dotenv from 'dotenv'
-import fs from 'fs'
 import { ICommand } from 'types'
 
 dotenv.config()
-const token = process.env.token
 
-const commands = new Collection<string, ICommand['execute']>()
-
-const client = new Client({
+const client = new PettyClient({
     intents: [
         'Guilds',
         'MessageContent',
@@ -21,24 +18,4 @@ client.on('ready', (client) => {
     console.log(`${client.user.username} ready`)
 })
 
-client.on('interactionCreate', interaction => {
-    if (!interaction.isCommand()) {
-        return
-    }
-
-    const commandHandler = commands.get(interaction.commandName)
-    if (commandHandler) {
-        commandHandler(interaction)
-    }
-});
-
-(async () => {
-    await Promise.all(fs.readdirSync('src/commands')
-        .filter(file => file.endsWith('ts'))
-        .map(async (file) => {
-            const command: ICommand = (await require(`./commands/${file}`)).default
-            commands.set(command.data.name, command.execute)
-        }))
-
-    client.login(token)
-})()
+client.login(process.env.token)
