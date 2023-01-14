d39a1965070fcf39f8e2fc05cfd087768419dd7c
Timur <timurtokaev.toombez@gmail.com>
2023-01-15 02:33:06 +0300

    Add getter for commandFiles

--- src/models/PettyClient.ts
+++ src/models/PettyClient.ts
@@ -2,5 +2,14 @@ import { Client, Collection } from "discord.js";
 import { ICommand } from "types";
 
 export default class PettyClient extends Client {
-    public commands: Collection<string, ICommand> = new Collection()
+    /**
+     * Client commands files
+     *
+     * Returns only files `js`/`ts` files that not starts with `_`
+     */
+    private get commandsFiles() {
+        return fs.readdirSync(PettyClient.COMMANDS_DIR)
+            .filter(file => file.endsWith('js') || file.endsWith('ts'))
+            .filter(file => !file.startsWith('_'))
+    }
 }
