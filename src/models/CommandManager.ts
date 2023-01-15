070e57d1cb6828963e325d97b10b18e79ec895ea
Timur <timurtokaev.toombez@gmail.com>
2023-01-15 04:13:17 +0300

    Add delete method to CommandManager

--- src/models/CommandManager.ts
+++ src/models/CommandManager.ts
@@ -1,6 +1,12 @@
-import { Guild } from "discord.js"
+import {
+    ApplicationCommandManager,
+    Guild,
+    GuildApplicationCommandManager
+} from "discord.js"
 import { ICommand } from "types"
 import PettyClient from "./PettyClient"
+
+type CommandsCollection = GuildApplicationCommandManager | ApplicationCommandManager
 
 export default class CommandManager {
     constructor(
@@ -18,11 +24,33 @@ export default class CommandManager {
 
         await this.client.application.commands.create(command.data, guild?.id)
     }
-        if (!this.client.application?.commands) return
 
-        const guildId = (typeof guild === 'string') ? guild : guild?.id
-        await this.client.application.commands.create(command.data, guildId)
+    /**
+     * Delete command in application or guild
+     *
+     * @param command target command
+     * @param guild guild where delete command
+     */
+    public async deleteCommand(command: ICommand, guild?: Guild): Promise<void> {
+        const collection: CommandsCollection | undefined = guild?.commands
+            || this.client.application?.commands
+        if (!collection) return
 
-        return
+        const _command = await this.findCommand(command, collection)
+        if (!_command) return
+
+        _command.delete()
+    }
+
+    /**
+     * Find command by name in collection
+     *
+     * @param command command to find
+     * @param collection collection where find
+     * @returns command
+     */
+    private async findCommand(command: ICommand, collection: CommandsCollection) {
+        await collection.fetch({})
+        return collection.cache.find(c => c.name === command.data.name)
     }
 }
