e9fa30f2e473ab55068ea85e4874845d84589cd5
Timur <timurtokaev.toombez@gmail.com>
2023-01-18 05:03:47 +0300

    Add example-bot app

---
+++ apps/example-bot/src/main.ts
@@ -0,0 +1,27 @@
+import {} from '@quvy/core'
+
+dotenv.config()
+
+const client = new QuviClient({
+    intents: [
+        'Guilds',
+        'MessageContent',
+        'GuildMembers',
+        'GuildMessages',
+    ]
+})
+
+const commandsCollection = new CommandsCollection()
+
+client.on('ready', async (_client) => {
+    console.log(`${_client.user.username} ready`)
+})
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isCommand()) return
+
+    const command = commandsCollection.get(interaction.commandName)
+    command?.execute(interaction)
+})
+
+client.login(process.env.token)
